5c8a987a2c5421e8d485204adeabcae962d72e9a
Armond <armonster@yandex.ru>
2021-11-18 22:09:55 +0300

    Add level, scripts , prefabs

---
+++ Assets/UiItem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiItem : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Text counterText;
+
+    private int count;
+
+    public void SetSprite(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+    public void SetCount(int count)
+    {
+        this.count = count;
+        counterText.text = count.ToString();
+    }
+
+    public void Decrease()
+    {
+       
+        if(--count == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            counterText.text = count.ToString();
+        }
+    }
+}
